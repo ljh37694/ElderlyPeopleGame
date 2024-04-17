6d0071bd3a4b9a127eabd3affbe84a258d4b5b48
i_junghoon <ljh37694@naver.com>
2024-04-17 14:14:18 +0900

    Bird가 5초마다 죽도록 함, 소리 나게 사는 것은 실패

--- EasyGemes/Assets/Scripts/Birds.cs
+++ EasyGemes/Assets/Scripts/Birds.cs
@@ -1,11 +1,33 @@
-using TMPro;
 using UnityEngine;
 
 public class Birds : MonoBehaviour {
+	float time;
+	const float ACTIVETIME = 5f;
+	AudioSource audioSource;
+	public AudioClip shotSound;
+
+	void Awake() {
+		time = 0f;
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update() {
+		if (gameObject.activeSelf) { 
+			time += Time.deltaTime;
+		}
+
+		if (time >= ACTIVETIME) {
+			gameObject.SetActive(false);
+			time = 0f;
+		}
+	}
+
 	void OnMouseEnter() {
 		if (!GameManager.instance.player.isGameOver()) {
-			gameObject.SetActive(false);
+			audioSource.PlayOneShot(shotSound);
 
+			gameObject.SetActive(false);
+			time = 0f;
 			GameManager.instance.score.PlusScore(100);
 		}
 	}
--- EasyGemes/Assets/Scripts/GameStart.cs
+++ EasyGemes/Assets/Scripts/GameStart.cs
@@ -4,6 +4,5 @@ using UnityEngine.SceneManagement;
 public class GameStart : MonoBehaviour {
 	public void GameStartBtn() {
 		SceneManager.LoadSceneAsync("CatchBird");
-		//SceneManager.UnloadScene();
 	}
 }
